e32324714edbab63e15ca23e5db9d3112b474313
HKristof25 <21.hajdu.kristof@mechwart.com>
2025-11-05 12:38:16 +0100

    DbContext

---
+++ SzakmakApi/Data/versenyzoDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace SzakmakApi.Data;
+using Microsoft.EntityFrameworkCore;
+using SzakmakApi.Models;
+{
+    public class versenyzoDbContext : DbContext
+    {
+    public versenyzoDbContext(DbContextOptions<versenyzoDbContext> options) : base(options) { }
+
+    public DbSet<orszag> orszagok {  get; set; }
+    
+    public DbSet<szakma> szakmak {  get; set; }
+
+    public DbSet<versenyzo> versenyzok { get; set; }
+
+    }
+}
